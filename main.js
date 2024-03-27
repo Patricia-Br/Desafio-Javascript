7f1bb9896f9f70c3c3b1963b298fa3b1c5e363ac
Patricia <124692201+Patricia-Br@users.noreply.github.com>
2024-03-27 17:35:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+// alert("Hello World!")
+
+
+
+// let numberFirst = Number(prompt("Digite o primeiro valor:"))
+// let numberSecond = Number(prompt("Digite agora o segundo valor:"))
+// let total = numberFirst + numberSecond
+// alert(`A soma dos dois valores é ${total}`)
+
+
+
+// const isNumber = 123
+// if (typeof isNumber == 'number') {
+//   alert('É um número')
+// } else {
+//   alert('Não é um número')
+// }
+
+
+
+// const isString = "string"
+// if (typeof isString == 'string') {
+//   alert("É uma string")
+// } else {
+//   alert("Não é uma string")
+// }
+
+
+
+// const isBoolean = true
+// if (typeof isBoolean == 'boolean') {
+//   alert("É um Boolean")
+// } else {
+//   alert("Não é um Boolean")
+// }
+
+
+
+// let numberOne = Number(prompt("Digite o primeiro valor: "))
+// let numberTwo = Number(prompt("Digite o segundo valor: "))
+// let total = numberOne - numberTwo
+// alert(`A subtração dos valores digitados é ${total}`)
+
+
+
+// let numberOne = Number(prompt("Digite o primeiro valor: "))
+// let numberTwo = Number(prompt("Digite o segundo valor: "))
+// let total = numberOne * numberTwo
+// alert(`A multiplicação dos valores digitados é ${total}`)
+
+
+
+// let numberOne = Number(prompt("Digite o primeiro valor: "))
+// let numberTwo = Number(prompt("Digite o segundo valor: "))
+// let total = numberOne / numberTwo
+// alert(`A divisão dos valores digitados é ${total}`)
+
+
+
+// let value = Number(prompt('Digite um número para verificar se é PAR ou não'))
+// if (value % 2 === 0) {
+//   alert(`O número digitado ${value}, é PAR!!!`)
+// } else {
+//   alert(`O valor digitado não é par`)
+// }
+
+
+
+// let value = Number(prompt('Digite um número para verificar se é ÍMPAR ou não'))
+// if (value % 2 === 1) {
+//   alert(`O número digitado ${value}, é ÍMPAR!!!`)
+// } else {
+//   alert(`O valor digitado não é ímpar`)
+// }
